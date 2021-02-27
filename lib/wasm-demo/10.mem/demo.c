694eb735e1cc32315c44b11904fff24c5999f846
flaxbin <zhengzhi883@gmail.com>
2021-02-27 22:58:42 +0800

    [+]mem

--- lib/wasm-demo/10.mem/demo.c
+++ lib/wasm-demo/10.mem/demo.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <emscripten/emscripten.h>
 
+typedef unsigned char uint8_t;
+
 int g_int = 42;
 double g_double = 3.1415926;
+uint8_t g_unit8[5] = 'dafsdf';
 
 int* EMSCRIPTEN_KEEPALIVE get_int_ptr() {
 	return &g_int;
@@ -12,8 +15,13 @@ double* EMSCRIPTEN_KEEPALIVE get_double_ptr() {
 	return &g_double;
 }
 
+uint8_t* EMSCRIPTEN_KEEPALIVE get_unit8_ptr() {
+	return &g_unit8;
+}
+
 void EMSCRIPTEN_KEEPALIVE print_data() {
 	printf("C {g_int:%d}\n", g_int);
 	printf("C {g_double:%lf}\n", g_double);
+	printf("C {g_unit8:%lf}\n", g_unit8);
 }
 
